79b62ada61887166a57b6c936edf203480e2844b
unknown <BT_4N2_04@BKTECH.LOCAL>
2024-01-16 14:06:23 -0500

    Initial commit

---
+++ src/FibonacciRunner.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+public class FibonacciRunner
+{
+    public static void main (String[] args)
+    {
+        // TEST constructor
+        System.out.println("--- TEST CONSTRUCTOR ---");
+        Fibonacci fib0 = new Fibonacci(2);
+        int[] seq0 = fib0.getSequence();
+        System.out.println(Arrays.toString(seq0));
+
+        Fibonacci fib1 = new Fibonacci(6);
+        int[] seq1 = fib1.getSequence();
+        System.out.println(Arrays.toString(seq1));
+
+        Fibonacci fib2 = new Fibonacci(15);
+        int[] seq2 = fib2.getSequence();
+        System.out.println(Arrays.toString(seq2));
+
+        // TEST getIndexOf
+        System.out.println("--- TEST getIndexOf ---");
+        System.out.println(fib2.getIndexOf(1));
+        System.out.println(fib2.getIndexOf(13));
+        System.out.println(fib2.getIndexOf(144));
+        System.out.println(fib2.getIndexOf(987));
+        System.out.println(fib2.getIndexOf(100));
+        System.out.println(fib2.getIndexOf(600));
+
+        // TEST extendBy
+        System.out.println("--- TEST extendBy ---");
+        fib0.extendBy(3);
+        System.out.println(Arrays.toString(fib0.getSequence()));
+        fib1.extendBy(5);
+        System.out.println(Arrays.toString(fib1.getSequence()));
+        fib2.extendBy(2);
+        System.out.println(Arrays.toString(fib2.getSequence()));
+
+        Fibonacci fib3 = new Fibonacci(5);
+        int[] seq3 = fib3.getSequence();
+        System.out.println(Arrays.toString(seq3));
+        seq3[0] = 100;
+        int[] seq4 = fib3.getSequence();
+        System.out.println(Arrays.toString(seq4));
+
+        /*
+        int[] fibonaccis = new int[3];
+        fibonaccis[0] = fib0;
+        fibonaccis[1] = fib1;
+        fibonaccis[2] = fib2;
+        for(Fibonacci fibonacci : fibonaccis)
+        {
+            System.out.println(fibonacci.getSequence().length);
+        }
+        */
+    }
+}
